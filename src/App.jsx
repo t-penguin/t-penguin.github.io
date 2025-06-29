7000d0a797133b883b10fd07520b29ec2a3327cd
Joseph <sourpina@gmail.com>
2025-06-29 15:49:02 -0400

    Added base About page

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./AppStyles.css";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import NavBar from "./components/NavBar";
+import About from "./pages/About";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
