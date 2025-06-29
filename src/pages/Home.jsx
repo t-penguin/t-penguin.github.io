67009538773452d1ca1abc861b8b250e23a537e9
Joseph <sourpina@gmail.com>
2025-06-29 16:41:34 -0400

    Added placeholder content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import PageHeader from "../components/PageHeader";
 
 const Home = () => {
   return (
-    <div className="homepage">
-      <h1>Home Page</h1>
+    <div className="content homepage">
+      <PageHeader headerText={"Frozen Rose Entertainment"} />
+      <p>
+        There isn't much to view here at the moment... <br />
+        <br />
+        Use the links on the navigation bar to check out my projects 
+        or learn more about me!
+      </p>
     </div>
   );
 };
