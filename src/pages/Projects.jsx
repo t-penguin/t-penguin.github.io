fbf4febe82c4a16d040669fdccbf169078853869
Joseph <sourpina@gmail.com>
2025-06-30 02:52:20 -0400

    Added sample project ImageCards

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import PageHeader from "../components/PageHeader";
+import ImageCard from "../components/ImageCard";
 
 const Projects = () => {
   return (
     <div className="content projects">
-      <PageHeader headerText={"Projects"}/>
+      <PageHeader headerText={"Projects"} />
+      <div className="card-container">
+        <div className="third">
+          <ImageCard src={"./Images/1920x1080.png"} alt={"Logo"} header={"Test Card"} text={"Test"}/>
+        </div>
+        <div className="third">
+          <ImageCard src={"./Images/1920x1080.png"} alt={"Logo"} header={"Test Card"} text={"Test"}/>
+        </div>
+        <div className="third">
+          <ImageCard src={"./Images/1920x1080.png"} alt={"Logo"} header={"Test Card"} text={"Test"}/>
+        </div>
+      </div>
     </div>
   );
 };
