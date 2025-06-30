a3e297583f0573463d5802d3f61a6776a9cf1101
Joseph <sourpina@gmail.com>
2025-06-30 12:40:32 -0400

    Added base Reflections project page

---
+++ src/pages/Reflections.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PageHeader from "../components/PageHeader";
+
+const Reflections = () => {
+  
+  return (
+    <div className="content reflections">
+      <PageHeader headerText={"Reflections Series"} />
+    </div>
+  );
+};
+
+export default Reflections;
