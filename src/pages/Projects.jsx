810b981a3dab5d7eaecae945cf712195faf97b7c
Joseph <sourpina@gmail.com>
2025-06-30 12:41:33 -0400

    Added Reflections and Pokemon project cards

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,22 +2,54 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import PageHeader from "../components/PageHeader";
 import ImageCard from "../components/ImageCard";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
+  const placeholder = {
+    img: "./Images/1920x1080.png",
+    alt: "Placeholder Image",
+    header: "Title",
+    text: "Hook Description"
+  }
+
+  const reflections = {
+    img: "./Images/1920x1080.png",
+    alt: "Placeholder Image",
+    header: "Reflections Series",
+    text: "A 3-part story about a world beyond ours"
+  };
+
+  const pokemonRB = {
+    img: "./Images/1920x1080.png",
+    alt: "Placeholder Image",
+    header: "Pokémon Red & Blue",
+    text: "A return to the Kanto Region"
+  }
+
   return (
     <div className="content projects">
       <PageHeader headerText={"Projects"} />
-      <div className="card-container">
+      <div className="card-container col3">
         <div className="third">
-          <ImageCard src={"./Images/1920x1080.png"} alt={"Logo"} header={"Test Card"} text={"Test"}/>
+          <Link to="reflections">
+            <ImageCard src={reflections.img} alt={reflections.alt}
+              header={reflections.header} text={reflections.text}/>
+          </Link>
         </div>
         <div className="third">
-          <ImageCard src={"./Images/1920x1080.png"} alt={"Logo"} header={"Test Card"} text={"Test"}/>
+          <ImageCard src={pokemonRB.img} alt={pokemonRB.alt}
+            header={pokemonRB.header} text={pokemonRB.text}/>
         </div>
         <div className="third">
-          <ImageCard src={"./Images/1920x1080.png"} alt={"Logo"} header={"Test Card"} text={"Test"}/>
+          <ImageCard src={placeholder.img} alt={placeholder.alt}
+            header={placeholder.header} text={placeholder.text}/>
+        </div>
+        <div className="third">
+          <ImageCard src={placeholder.img} alt={placeholder.alt}
+            header={placeholder.header} text={placeholder.text}/>
         </div>
       </div>
+      <div className="horizontal-divider"></div>
     </div>
   );
 };
