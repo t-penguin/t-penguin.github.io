74ec90968ec8f14f096de2f94c12b1c389ffb2fb
Joseph <sourpina@gmail.com>
2025-06-30 12:40:48 -0400

    Changed Links to NavLinks

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./NavBarStyles.css";
 
 const NavBar = () => {
   return (
     <nav>
       <div className="left">
-        <Link to="/" className="logo">
+        <NavLink to="/" className="logo">
           <ul>
             <li><img src="./Images/Frozen_Rose.png"></img></li>
             <li><span>Frozen Rose</span></li>
           </ul>
-        </Link>
+        </NavLink>
       </div>
       <div className="right">
         <ul>
-          <li><Link to="/projects"><span>Projects</span></Link></li>
-          <li><Link to="/about"><span>About</span></Link></li>
+          <li><NavLink to="/projects"><span>Projects</span></NavLink></li>
+          <li><NavLink to="/about"><span>About</span></NavLink></li>
         </ul>
       </div>
     </nav>
