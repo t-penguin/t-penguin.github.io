5cf6991cf9719ef175346f1630713059d80d3711
Joseph <sourpina@gmail.com>
2025-07-01 10:39:49 -0400

    Added Origins subpage

---
+++ src/pages/subpages/Origins.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SectionHeader } from "../../components/Headers";
+
+const Origins = () => {
+  return (
+    <div className="origins">
+      <SectionHeader headerText={"In The Beginning"} centered={false} large={false} />
+    </div>
+  );
+};
+
+export default Origins;
