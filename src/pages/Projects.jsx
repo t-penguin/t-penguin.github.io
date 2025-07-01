b95046d6b3562edd52b4bceac8762c43920e2cad
Joseph <sourpina@gmail.com>
2025-07-01 10:39:00 -0400

    Changed PageHeader import path

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PageHeader from "../components/PageHeader";
+import { PageHeader } from "../components/Headers";
 import ImageCard from "../components/ImageCard";
 import { Link } from "react-router-dom";
 
